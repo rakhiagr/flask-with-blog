7fb65b62fe83213a837ae470aa9c5d0f1721cc23
rakhiagr <rakhiagr2020@gmail.com>
2020-09-24 20:58:54 +0530

    Added few lines

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -22,6 +22,8 @@ def create_app(config_class = Config):
 	bcrypt.init_app(app)
 	login_manager.init_app(app)
 	mail.init_app(app)
+	db.create_all()
+	db.session.commit()
 
 	from flaskblog.users.routes import users
 	from flaskblog.posts.routes import posts
